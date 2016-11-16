1428080d6bd2e91be1e2042c6bd021ea4af161c5
MervinPraison <praison.ms@gmail.com>
2016-11-16 15:06:02 +0000

    category update working

--- ajax-form.php
+++ ajax-form.php
@@ -18,7 +18,8 @@ function ajax_form_enqueue_scripts() {
 	wp_enqueue_script( 'formset', plugins_url( '/form.js', __FILE__ ), array('jquery'), '1.0', true );
 
 	wp_localize_script( 'formset', 'catform', array(
-		'ajax_url' => admin_url( 'admin-ajax.php' )
+		'ajax_url' => admin_url( 'admin-ajax.php' ),
+  		'ajax_nonce' => wp_create_nonce('name_of_nonce_field'),
 	));
 
 }
@@ -30,14 +31,45 @@ function ajax_form_display( $content ) {
 	if ( is_single() ) {
 		
 
+$prev_cat_id = get_post_meta( get_the_ID(), 'form_cat', true );
+$term = get_term($prev_cat_id);
 
-$value =<<<EOT
-<form class="form" method="post" id="ajax-contact-form" action="/aah/wp-admin/admin-ajax.php?action=contact_form">
-<input type="text" name="name" id="name"  placeholder="Name" required="">
-<button type="submit" class="btn">Submit</button></form>
-EOT;
+$args = array(
+	'show_option_all'    => '',
+	'show_option_none'   => '',
+	'option_none_value'  => '-1',
+	'orderby'            => 'ID',
+	'order'              => 'ASC',
+	'show_count'         => 0,
+	'hide_empty'         => 1,
+	'child_of'           => 0,
+	'exclude'            => '',
+	'include'            => '',
+	'echo'               => 0,
+	'selected'           => $prev_cat_id,
+	'hierarchical'       => 0,
+	'name'               => 'cat_name',
+	'id'                 => 'cat_name',
+	'class'              => 'cat_name',
+	'depth'              => 0,
+	'tab_index'          => 0,
+	'taxonomy'           => 'category',
+	'hide_if_empty'      => false,
+	'value_field'	     => 'term_id',
+);
 
-$value .= '<br /><span id="form-output">Output</span>';
+$value ='
+<form class="form" method="post" id="ajax-contact-form" action="/aah/wp-admin/admin-ajax.php">
+<input type="hidden" name="action" value="contact_form">
+<input type="hidden" name="post_id" id="post_id" value="'.get_the_ID().'">';
+$value .= wp_dropdown_categories($args);
+//$value .= wp_nonce_field( 'contact_form_nonce_2', 'name_of_nonce_field' );
+$value .= '<button type="submit" class="btn">Submit</button></form>';
+
+
+
+
+$value .= '<br /><span id="cat-form-output">'.$term->name.'</span>';
 
 	}
 
@@ -50,6 +82,34 @@ add_action('wp_ajax_nopriv_contact_form', 'contact_form');
 
 function contact_form()
 {
-echo $_REQUEST['name'];   
-die(); 
+
+    if (
+    	!wp_verify_nonce( $_REQUEST['security'], 'name_of_nonce_field')
+    	) {
+
+        exit('The form is not valid'); 
+    }
+ 
+    // ... Processing further
+
+
+	$cat_name = $_REQUEST['cat_name']; 
+	$post_id = $_REQUEST['post_id'];
+	$term = get_term($cat_name);
+
+	// $prev_cat_name = get_post_meta( $post_id, 'form_cat', true );
+
+	if($cat_name&&$post_id) {
+		update_post_meta( $post_id, 'form_cat', $cat_name );
+		wp_set_post_categories( $post_id, $cat_name, false );
+	}
+
+	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) { 
+		echo $term->name. ' <span style="color:LightGreen;" >Updated</span>';
+		die();
+	}
+	else {
+		wp_redirect( get_permalink( $_REQUEST['post_id'] ) );
+		exit();
+	}
 }
--- form.js
+++ form.js
@@ -1,18 +1,25 @@
+jQuery(document).ready(function($) {
+
 jQuery('#ajax-contact-form').submit(function(e){
-    var name = jQuery("#name").val();
+    var cat_name = jQuery("select#cat_name option:selected").val();
+    var post_id = jQuery("#post_id").val();
     jQuery.ajax({ 
-         url: catform.ajax_url,
+         url: catform.ajax_url,         
 		 type: 'post',
          data: {
          		action: 'contact_form', 
-         		name:name
+         		cat_name:cat_name,
+         		post_id:post_id,
+         		security: catform.ajax_nonce
          },
          success: function(response) {
-              jQuery('#cat-count').html( response ); //should print out the name since you sent it along
+              jQuery('#cat-form-output').html( response ); //should print out the name since you sent it along
 
         }
     });
 
     return false;
+
+});
 
 });
